d7092deb12829d364cb8fa872ae1f5f775ee951e
Daniel Raj <68500777+raj-daniel@users.noreply.github.com>
2022-10-09 02:19:21 +0530

    added index for all modals

--- src/controllers/wishlistController.js
+++ src/controllers/wishlistController.js
@@ -1,57 +1,70 @@
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
-const Wishlist = require('./../model/wishlistModel')
+const Wishlist = require('./../model/wishlistModel');
+const { find } = require('lodash');
+const { findOne } = require('../model/userModel');
 
-exports.getAllWishlist = catchAsync (async (req,res,next)=>{
-    const allWishlist = await Wishlist.find({userId:req.params.id})
+exports.getAllWishlist = catchAsync(async (req, res, next) => {
+  const allWishlist = await Wishlist.find({ userId: req.params.id });
 
-    if(!allWishlist){return new AppError('No wishlist found', 400)}
+  if (!allWishlist) {
+    return new AppError('No wishlist found', 400);
+  }
 
-    res.status(200).json({
-        status: 'success',
+  res.status(200).json({
+    status: 'success',
     data: {
-        allWishlist
+      allWishlist,
     },
-    })
-})
-exports.getAllWishlistData = catchAsync (async (req,res,next)=>{
-    const allWishlist = await Wishlist.find({userId:req.params.id}).populate({path:'rentalId', select:['rentalName','subDestination','price','state','originalImages','noOfPeopleAccomodate']})
+  });
+});
+exports.getAllWishlistData = catchAsync(async (req, res, next) => {
+  const allWishlist = await Wishlist.find({ userId: req.params.id }).populate({
+    path: 'rentalId',
+    select: ['rentalName', 'subDestination', 'price', 'state', 'originalImages', 'noOfPeopleAccomodate'],
+  });
 
-    if(!allWishlist){return new AppError('No wishlist found', 400)}
+  if (!allWishlist) {
+    return new AppError('No wishlist found', 400);
+  }
 
-    res.status(200).json({
-        status: 'success',
+  res.status(200).json({
+    status: 'success',
     data: {
-        allWishlist
+      allWishlist,
     },
-    })
-})
+  });
+});
 
-exports.addToWishlist = catchAsync (async (req,res,next)=>{
-    const wishlist = await Wishlist.create({
-        userId: req.body.userId,
-        rentalId: req.body.rentalId,
-    })
+exports.addToWishlist = catchAsync(async (req, res, next) => {
+  const wishlist = await Wishlist.create({
+    userId: req.body.userId,
+    rentalId: req.body.rentalId,
+  });
 
-    if(!wishlist){return new AppError('Error creating a wishlist', 400)}
+  if (!wishlist) {
+    return new AppError('Error creating a wishlist', 400);
+  }
 
-    res.status(201).json({
-        status: 'success',
+  res.status(201).json({
+    status: 'success',
     data: {
-        wishlist
+      wishlist,
     },
-    })
-})
+  });
+});
 
-exports.deleteFromWishlist = catchAsync (async (req,res,next)=>{
-    const wishlist = await Wishlist.findByIdAndDelete({_id:req.params.id})
+exports.deleteFromWishlist = catchAsync(async (req, res, next) => {
+  const wishlist = await Wishlist.findOneAndDelete({ _id: req.params.id });
 
-    if(!wishlist){return new AppError('Something went wrong trouble removing the wishlist', 400)}
+  if (!wishlist) {
+    return new AppError('Something went wrong trouble removing the wishlist', 400);
+  }
 
-    res.status(204).json({
-        status: 'success',
+  res.status(204).json({
+    status: 'success',
     data: {
-        wishlist
+      wishlist,
     },
-    })
-})
+  });
+});
